09ce8f3472407461406ef9028572ba4e20daeacd
wnd. <wndslr@proton.me>
2026-05-18 02:48:12 +0200

    upd cart.js

--- src/api/endpoints/cart.js
+++ src/api/endpoints/cart.js
@@ -1,7 +1,7 @@
 import api from '../axios.js'
 
-export const fetchCart    = ()           => api.get('/cart')
-export const addCartItem  = (item)       => api.post('/cart', item)
+export const fetchCart = () => api.get('/cart')
+export const addCartItem = (item) => api.post('/cart', item)
 export const updateCartItem = (id, data) => api.put(`/cart/${id}`, data)
-export const deleteCartItem = (id)       => api.delete(`/cart/${id}`)
-export const clearCart    = ()           => api.delete('/cart')
+export const deleteCartItem = (id) => api.delete(`/cart/${id}`)
+export const clearCart = () => api.delete('/cart')
