557e830300c27e5b53c473e6c81f7e182632e9b8
wndslr <wndslr@proton.me>
2026-05-22 03:08:06 +0200

    lesson7-9

--- frontend/api/endpoints/product.js
+++
@@ -1,3 +0,0 @@
-import api from '../axios.js'
-
-export const fetchProduct = (id) => api.get(`/product/${id}`)
--- frontend/api/endpoints/user.js
+++
@@ -1,4 +0,0 @@
-import api from '../axios.js'
-
-export const fetchUser  = ()     => api.get('/user')
-export const registerUser = (data) => api.post('/user/register', data)
